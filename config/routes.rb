bb0e2055e439f4c38ee4e10660d9de3c61b47e11
Christian Hjalmarsson <christian-hjalmarsson@hotmail.com>
2017-01-08 14:16:04 +0100

    User signup: A first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  #get 'static_pages/about'
+  #get 'static_pages/contact'
 
-  get 'static_pages/help'
+  root to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  #match '/help', to: 'static_pages#help'
+  #match '/home', to: 'static_page#home'
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
